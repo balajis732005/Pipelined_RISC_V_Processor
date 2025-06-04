3357030338b71faafb5d32695749aec7d801bf2e
balajis732005 <balajis07032005@gmail.com>
2025-06-05 01:31:03 +0530

    Write_Back_Check

--- src/WriteBack/WriteBackMux/writeBackMux.sv
+++ src/WriteBack/WriteBackMux/writeBackMux.sv
@@ -1,12 +1,13 @@
 `include "../../Mux2_1/mux2_1.sv";
 
 module writeBackMux(
-    input logic [31:0] aluData,
-    input logic [31:0] memoryData,
-    input logic writeBackMemoryOrAlu,
-    output logic [31:0] dataBack
+    input logic [31:0]  aluData,              // ALU OUTPUT DATA
+    input logic [31:0]  memoryData,           // MEMORY READ DATA
+    input logic         writeBackMemoryOrAlu, // WRITE BACK SELECT LINE
+    output logic [31:0] dataBack              // DATA BACK TO REGISTER
 );
 
+    // 2 x 1 MUX
     mux2_1 m1(
         .in1(aluData),
         .in2(memoryData),
