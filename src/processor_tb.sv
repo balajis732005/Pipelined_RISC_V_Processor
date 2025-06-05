0907a331994da2a651a48c2f1c6c97f769bb0e08
balajis732005 <balajis07032005@gmail.com>
2025-06-05 11:03:00 +0530

    TB

--- src/processor_tb.sv
+++ src/processor_tb.sv
@@ -1,5 +1,25 @@
 module processor_tb;
 
-    
+    logic clock;
+    logic reset;
+
+    processor processorDut(
+        .clock(clock),
+        .reset(reset)
+    );
+
+    initial begin
+        clock = 1'b0;
+        reset = 1'b1;
+    end
+
+    always begin
+        #5 clock = ~clock;
+    end
+
+    initial begin
+        #10 reset = 1'b0;
+        #200 $finish();
+    end
 
 endmodule
