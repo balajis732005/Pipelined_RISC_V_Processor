1b2dc6578ddce8880dd22bfa2b30b50e9fdc86b1
balajis732005 <balajis07032005@gmail.com>
2025-06-05 09:15:10 +0530

    Decode_Processor_Add

--- src/Decode/InstructionDecoder/instructionDecoder.sv
+++ src/Decode/InstructionDecoder/instructionDecoder.sv
@@ -2,17 +2,17 @@ module instructionDecoder(
     input  logic [31:0] instruction,  // INSTRUCTION
     output logic [6:0]  opcode,       // OPCODE
     output logic [4:0]  rd,           // DESTINATION REGISTER
-    output logic [2:0]  funct3,       // FUNC3 OPERATION
+    output logic [2:0]  func3,       // FUNC3 OPERATION
     output logic [4:0]  rs1,          // SOUREC REGISTER 1
     output logic [4:0]  rs2,          // SOUREC REGISTER 2
-    output logic [6:0]  funct7        // FUNC7 OPERATION
+    output logic [6:0]  func7        // FUNC7 OPERATION
 );
 
     assign opcode = instruction[6:0];
     assign rd     = instruction[11:7];
-    assign funct3 = instruction[14:12];
+    assign func3 = instruction[14:12];
     assign rs1    = instruction[19:15];
     assign rs2    = instruction[24:20];
-    assign funct7 = instruction[31:25];
+    assign func7 = instruction[31:25];
 
 endmodule
--- src/processor.sv
+++ src/processor.sv
@@ -7,6 +7,7 @@
 `include "Decode\RegisterFile\registerFile.sv"
 `include "Decode\ControlUnit\controlUnit.sv"
 `include "Decode\ImmediateGenerator\immediateGenerator.sv"
+`include "PipelineRegisters\DecodeToExecuteRegister\decodeToExecuteRegister.sv"
 
 module processor(
     input logic clock,
@@ -21,30 +22,52 @@ module processor(
     logic [31:0] pcOut;
     logic [31:0] currentInstruction;
 
-    //FETCH TO DECODE REGISTER
-    logic [31:0] pcOutReg;
+    // FETCH TO DECODE REGISTER
+    logic [31:0] pcOutRegFetch;
     logic [31:0] instructionReg;
 
-    //DECODE
-    logic [6:0] opcode;
-    logic [4:0] rd;
-    logic [2:0] funct3;
-    logic [4:0] rs1;
-    logic [4:0] rs2;
-    logic [6:0] funct7;
-    logic       pcUpdate;
-    logic       memoryReadEnable;
-    logic       memoryWriteEnable;
-    logic       registerWriteEnable;
-    logic [1:0] aluSrc1;
-    logic [1:0] aluSrc2;
-    logic [2:0] aluOperation;
-    logic       pcAdderSrc;
-    logic       writeBackFromAluOrMemory;
+    // DECODE
+    logic [6:0]  opcode;
+    logic [4:0]  rd;
+    logic [2:0]  func3;
+    logic [4:0]  rs1;
+    logic [4:0]  rs2;
+    logic [6:0]  func7;
+    logic        pcUpdate;
+    logic        memoryReadEnable;
+    logic        memoryWriteEnable;
+    logic        registerWriteEnable;
+    logic [1:0]  aluSrc1;
+    logic [1:0]  aluSrc2;
+    logic [2:0]  aluOperation;
+    logic        pcAdderSrc;
+    logic        writeBackFromAluOrMemory;
     logic [31:0] writeData;
     logic [31:0] readData1;
     logic [31:0] readData2;
     logic [31:0] immediateValue;
+
+    // DECODE TO EXECUTE REGISTER
+    logic [31:0] pcOutRegDecode;
+    logic [31:0] readData1OutReg;
+    logic [31:0] readData2OutReg;
+    logic [31:0] immediateValueOutReg;
+    logic [4:0]  rs1OutReg;
+    logic [4:0]  rs2OutReg;
+    logic [4:0]  rdOutReg;
+    logic [2:0]  func3OutReg;
+    logic [6:0]  func7OutReg;
+    logic        pcUpdateOutReg;
+    logic        memoryReadEnableOutReg;
+    logic        memoryWriteEnableOutReg;
+    logic        registerWriteEnableOutReg;
+    logic [1:0]  aluSrc1OutReg;
+    logic [1:0]  aluSrc2OutReg;
+    logic [2:0]  aluOperationOutReg;
+    logic        pcAdderSrcOutReg;
+    logic        writeBackFromMemoryOrAluOutReg;
+
+    // EXECUTE
 
     /* --------------------------------------------------FETCH------------------------------------------------- */
 
@@ -79,7 +102,7 @@ module processor(
         .reset(reset),
         .pc(pcOut),
         .instruction(currentInstruction),
-        .pcOut(pcOutReg),
+        .pcOut(pcOutRegFetch),
         .instOut(instructionReg)
     );
 
@@ -89,7 +112,7 @@ module processor(
         .instruction(instructionReg),
         .opcode(opcode),
         .rd(rd),
-        .funct3(funct3),
+        .func3(func3),
         .rs1(rs1),
         .rs2(rs2),
         .func7(func7)
@@ -126,6 +149,49 @@ module processor(
     );
 
     /* ------------------------------------------DECODE TO EXECUTE REGISTER-----------------------------------------*/
+
+    decodeToExecuteRegister decodeToExecuteRegisterDut(
+        .clock(clock),
+        .reset(reset),
+        .pc(pcOutRegFetch),
+        .readData1(readData1),
+        .readData2(readData2),
+        .immediateValue(immediateValue),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .func3(func3),
+        .func7(func7),
+        .pcUpadte(pcUpdate),
+        .memoryReadEnable(memoryReadEnable),
+        .memoryWriteEnable(memoryWriteEnable),
+        .registerWriteEnable(registerWriteEnable),
+        .aluSrc1(aluSrc1),
+        .aluSrc2(aluSrc2),
+        .aluOperation(aluOperation),
+        .pcAdderSrc(pcAdderSrc),
+        .writeBackFromMemoryOrAlu(writeBackFromMemoryOrAlu),
+        .pcOut(pcOutRegDecode),
+        .readData1Out(readData1OutReg),
+        .readData2Out(readData2OutReg),
+        .immediateValueOut(immediateValueOutReg),
+        .rs1Out(rs1OutReg),
+        .rs2Out(rs2OutReg),
+        .rdOut(rdOutReg),
+        .func3Out(func3OutReg),
+        .func7Out(func7OutReg),
+        .pcUpdateOut(pcUpdateOutReg),
+        .memoryReadEnableOut(memoryReadEnableOutReg),
+        .memoryWriteEnableOut(memoryWriteEnableOutReg),
+        .registerWriteEnableOut(registerWriteEnableOutReg),
+        .aluSrc1Out(aluSrc1OutReg),
+        .aluSrc2Out(aluSrc2OutReg),
+        .aluOperationOut(aluOperationOutReg),
+        .pcAdderSrcOut(pcAdderSrcOutReg),
+        .writeBackFromMemoryOrAluOut(writeBackFromMemoryOrAluOutReg)
+    );
+
+    /* ------------------------------------------------EXECUTE-------------------------------------------------------*/
 
     
     
