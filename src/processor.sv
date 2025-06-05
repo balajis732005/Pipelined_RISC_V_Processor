43ec0818cf59d7c04a6d5aac5b34d24ff2c2931a
balajis732005 <balajis07032005@gmail.com>
2025-06-05 08:32:26 +0530

    Fetch_Call_in_processor

--- src/processor.sv
+++ src/processor.sv
@@ -1,3 +1,44 @@
-module processor();
+`include "Fetch\ProgramCounterInputMux\programCounterInputMux.sv"
+`include "Fetch\ProgramCounter\programCounter.sv"
+`include "Fetch\ProgramCounterIncrementor\programCounterIncrementor.sv"
+`include "Fetch\InstructionMemory\instructionMemory.sv"
+
+module processor(
+    input logic clock,
+    input logic reset
+);
+
+    logic [31:0] pcPlus;
+    logic [31:0] pcJump;
+    logic        pcIJSel;
+    logic [31:0] pcVal;
+    logic [31:0] pcInst;
+    logic [31:0] currentInst;
+
+    programCounterInputMux programCounterInputMuxDut(
+        .pcIncrement(pcPlus),
+        .pcJump(pcJump),
+        .pcIncrementOrJump(pcIJSel),
+        .pcInput(pcVal)
+    );
+
+    programCounter programCounterDut(
+        .clock(clock),
+        .reset(reset),
+        .pcIn(pcVal),
+        .pcOut(pcInst)
+    );
+
+    programCounterIncrementor programCounterIncrementorDut(
+        .pcCurrent(pcInst),
+        .pcNext(pcPlus)
+    );
+
+    instructionMemory instructionMemoryDut(
+        .instructionAddress(pcInst),
+        .instruction(currentInst)
+    );
+
+    
     
 endmodule
